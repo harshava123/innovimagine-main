b0904126a53013b4f9d92ea83371c856e0e745fa
harshava123 <harshav123.mru@gmail.com>
2025-08-29 11:44:17 +0530

    bruio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import Layout from './components/layout/Layout'
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
     <>
       <Layout/>
-      <Analytics />
-      <SpeedInsights />
     </>
   )
 }
