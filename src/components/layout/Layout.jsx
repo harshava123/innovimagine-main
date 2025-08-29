7da2d94661b7575fc8f627f4f97dc1661829d431
harshava123 <harshav123.mru@gmail.com>
2025-08-29 12:21:58 +0530

    issuefix

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Routers from '../routers/Routers'
-import { BrowserRouter as Router, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
 import ScrollToTop from '../ScrollToTop'
-
-// Component to track route changes
-const RouteTracker = () => {
-  const location = useLocation();
-  
-  useEffect(() => {
-    // Track page view when route changes
-    if (window.va) {
-      window.va.track('page_view', {
-        url: window.location.href,
-        path: location.pathname
-      });
-      console.log('Route change tracked:', location.pathname);
-    }
-  }, [location]);
-  
-  return null;
-};
 
 function Layout() {
   return (
     <Router>
         <ScrollToTop />
-        <RouteTracker />
         <Header/>
         <Routers/>
         <Footer/>
